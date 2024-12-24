45607caa6533c7ffcf5028a14a0559c0ee274792
Volodymyr Hrynevych <vhrynevich20@gmail.com>
2024-12-24 16:20:39 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+      <>
+          <div className="header">
+              <h1>Hello, I'm [Your Name]</h1>
+          </div>
+          <div className="card">
+              <p>
+                  I'm a Java developer, passionate about java development.
+              </p>
+              <p>
+                  This site is currently under construction. Check back soon for more updates!
+              </p>
+          </div>
+      </>
+  )
+}
+
+export default App
