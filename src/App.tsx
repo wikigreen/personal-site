19cb4fedb4d5d27a1c623c20eb8a4875719e774f
Volodymyr Hrynevych <vhrynevich20@gmail.com>
2024-12-24 16:21:58 +0000

    Install prettier and run it

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
-      <>
-          <div className="header">
-              <h1>Hello, I'm [Your Name]</h1>
-          </div>
-          <div className="card">
-              <p>
-                  I'm a Java developer, passionate about java development.
-              </p>
-              <p>
-                  This site is currently under construction. Check back soon for more updates!
-              </p>
-          </div>
-      </>
-  )
+    <>
+      <div className="header">
+        <h1>Hello, I'm [Your Name]</h1>
+      </div>
+      <div className="card">
+        <p>I'm a Java developer, passionate about java development.</p>
+        <p>
+          This site is currently under construction. Check back soon for more
+          updates!
+        </p>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
